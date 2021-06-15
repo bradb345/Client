00a8d461a8fa58be4f944e1174d328d3805c079c
bradb345 <bradbernard17@gmail.com>
2021-06-15 14:56:18 +0100

    organization

--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -5,6 +5,9 @@ import { faHome } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function NavBar() {
+
+  
+
   return (
     <>
       <nav>
