d40e0918f79bc25e21ed32c1496c98b643c08110
“gursham” <Gursham2001@hotmail.com>
2021-06-21 16:32:06 +0100

    loading spinner

--- src/components/users/UserIndex.js
+++ src/components/users/UserIndex.js
@@ -2,9 +2,14 @@ import React from 'react'
 import UserCard from '../users/UserCard.js'
 import { getAllUsers } from '../lib/api'
 
-function UserIndex({ searchTerm }) {
+import Error from '../common/Error.js'
 
+import Loader from 'react-loader-spinner'
+
+function UserIndex({ searchTerm }) {
   const [users, setUsers] = React.useState(null)
+  const [isError, setIsError] = React.useState(false)
+  const isLoading = !users && !isError
 
   React.useEffect(() => {
     const getData = async () => {
@@ -13,6 +18,7 @@ function UserIndex({ searchTerm }) {
         setUsers(response.data)
         console.log(response.data)
       } catch (error) {
+        setIsError(true)
         console.log(error)
       }
     }
@@ -29,9 +35,19 @@ function UserIndex({ searchTerm }) {
   }
 
   return (
+    <>
+      <div className="ProjectIndex-Container">
+        {isError && <Error />}
+        {isLoading && <Loader
+          type="TailSpin"
+          color="#F70C0C"
+          height={100}
+          width={100} //3 secs
+        />}
 
-    <div className="ProjectIndex-Container">
-      { users &&
+
+        <div className="ProjectIndex-Container">
+          { users &&
         filterUsers().map((user) => (
 
           <UserCard
@@ -49,8 +65,10 @@ function UserIndex({ searchTerm }) {
           />
 
         ))
-      }
-    </div>
+          }
+        </div>
+      </div>
+    </>
   )
 }
 
